ffb13ffe53faeffcb89d8bcbbd51ad547acfeca1
kisshead <santiwansui@gmial.com>
2024-03-09 19:29:35 +0800

    Site updated: 2024-03-09 19:29:31

--- html/index.js
+++ html/index.js
@@ -1,4 +1,4 @@
-var S = {
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>var S = {
     init: function() {
         var action = window.location.href,
             i = action.indexOf('?a=');
